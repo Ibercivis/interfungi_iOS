42bcd06f78632ace1db9bde3a3509a768c36426b
jbarbaborderias <jbarba@bifi.es>
2022-06-06 10:42:23 +0200

    Initial Commit

---
+++ interfungi_iOS/interfungi_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  interfungi_iOSApp.swift
+//  interfungi_iOS
+//
+//  Created by Ibercivis on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct interfungi_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
